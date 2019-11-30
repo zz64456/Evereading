48aa58d2da185ef5332a64dbf09577b748bd9e6d
lchuang <z5202dbc@gmail.com>
2019-11-29 21:29:36 -0800

    Command suggested_key
    USE Ctrl+Shift+Y to switch bg-colof of facebook homepage
    Adapt:
    event.js, manifest.json

--- event.js
+++ event.js
@@ -1,7 +1,44 @@
-console.log("background page ready");  
+var rule = {
+    //條件
+    conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+            //url的匹配指定
+            pageUrl: { 
+                hostEquals: 'www.google.com.tw', 
+                schemes: ['https']
+            }
+        })
+    ],
+    //動作：啟用頁面按鈕
+    actions: [new chrome.declarativeContent.ShowPageAction()]
+};
 
-chrome.browserAction.onClicked.addListener(function(tab) {  
-    // console.log(tab);  
-    alert("使用者在"+tab.title+ "中點擊了瀏覽器按鈕");  
 
+//刪除規則，並重新註冊
+chrome.runtime.onInstalled.addListener(function(details) {
+    //移除所有舊規則
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        //註開新規則
+        console.log('remove completed!');
+        chrome.declarativeContent.onPageChanged.addRules([rule]);
+    });
+});
+
+var toggleBg = false;
+
+chrome.commands.onCommand.addListener(function(command) {
+    console.log('Command:', command);
+    if(command == "switch-fb-bg" && toggleBg){
+		chrome.tabs.executeScript({
+		    code: 'document.body.style.backgroundColor="red"'
+		}); 
+
+		toggleBg = !toggleBg;	
+    }
+    else if (command == "switch-fb-bg" && !toggleBg) {
+		chrome.tabs.executeScript({
+		    code: 'document.body.style.backgroundColor="black"'
+		}); 
+    	toggleBg = !toggleBg;	
+    }
 });
